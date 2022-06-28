ffde27af1484614e595edcc99fb4fce7457acb01
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-06-28 16:45:11 +0100

    new Commit

--- task/task/settings.py
+++ task/task/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'api.apps.ApiConfig', 
+    'api.apps.ApiConfig', #declaration of the app :
     'rest_framework'
 ]
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'task.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join("templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
