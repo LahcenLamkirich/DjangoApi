50c9d8a536427d8cebddda8a642403dc2ae182c4
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-06-29 02:33:51 +0100

    commit

--- task/api/models.py
+++ task/api/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, blank=True, null=True)
-
+    date_published = models.DateTimeField("Date published")
     def __str__(self):
         return '%s %s' % (self.title, self.completed)
